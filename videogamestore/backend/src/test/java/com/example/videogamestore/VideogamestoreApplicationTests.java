5fd88d8ed06bcd7e9875bfe9a19b0f0bdb021cb7
Christopher Brown <quimby2999@yahoo.com>
2023-09-24 14:30:32 -0400

    initial commit

---
+++ videogamestore/backend/src/test/java/com/example/videogamestore/VideogamestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.videogamestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideogamestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
