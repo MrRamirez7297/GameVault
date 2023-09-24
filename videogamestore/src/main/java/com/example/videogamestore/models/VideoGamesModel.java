57ca0114f7fbcffb531da77a325c672ffb569522
Christopher Brown <quimby2999@yahoo.com>
2023-09-24 15:41:13 -0400

    initial fixes

---
+++ videogamestore/src/main/java/com/example/videogamestore/models/VideoGamesModel.java
@@ -0,0 +1,15 @@
+package com.example.videogamestore.models;
+
+@Entity
+public class VideoGamesModel {
+    
+    @Id
+    @Generated Value
+    private Long id;
+
+    private String name:
+
+    
+
+
+}
