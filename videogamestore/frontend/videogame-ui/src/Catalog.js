9c9f601c94761aced8c53707920cc1d8c69fabe0
Ahmad Omo-abu <Ahmadomo716@gmail.com>
2023-10-07 14:41:48 -0400

    update

--- videogamestore/frontend/videogame-ui/src/Catalog.js
+++ videogamestore/frontend/videogame-ui/src/Catalog.js
@@ -1,36 +1,60 @@
+/* eslint-disable no-restricted-globals */
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
+import GamePage from "./GamePage";
+import { Link, Route, Router, Routes } from "react-router-dom";
 
 function Catalog() {
-    const [Catalog, setCatalog] = useState([]);
+  const [Catalog, setCatalog] = useState([]);
 
-    useEffect(() => {
-        async function fetchCatalog() {
-            try {
-                const { data } = await axios.get("http://localhost:8080/all-games");
-                setCatalog(data);
-            } catch (error) {
-                console.error("Error fetching Catalog: ", error);
-            }
-        }
+  useEffect(() => {
+    async function fetchCatalog() {
+      try {
+        const { data } = await axios.get("http://localhost:8080/all-games");
+        setCatalog(data);
+      } catch (error) {
+        console.error("Error fetching Catalog: ", error);
+      }
+    }
 
-        fetchCatalog();
-    }, [Catalog]);
+    fetchCatalog();
+  }, [Catalog]);
 
-    return (
+  return (
+    <div class="game-container">
+      <h1>Game Library goes here! </h1>
+      <div id="game-library-box">
+        <ul id="game-items">
+          {Catalog.map((game) => (
+            <li id="game-items" key={game.id}>
+              {" "}
+              <div class="game-library-list">
+                <div class="game-library-items" onClick={() => GamePage()}>
+                  <h2 id="game-title">{game.name}</h2>
 
+                  <div id="game-image">
+                    <img
+                      id="idk"
+                      src={game.imageUrl}
+                      alt={game.imageUrl}
+                      width="300"
+                    />
+                  </div>
+                  <div>
+                    <p2 id="game-info">{game.description}</p2>
+                  </div>
+                  
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <Link to='/game-page'>More Info.</Link>
+      </div>
 
-        <div class="game-container">
-            <h1>Game Library goes here! </h1>
-            <div id="game-library-box">
-                <div class="games">1</div>
-                <div class="games">2</div>
-                <div class="games">3</div>
-                <div class="games">4</div>
-            </div>
-        </div>
-    );
+    </div>
+  );
 }
 export default Catalog;
