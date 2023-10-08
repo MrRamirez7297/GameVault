12ae9e7ddff738b95b3d9d76673617b39ea0e7f8
tyrese jone <tyersejones230@yahoo.com>
2023-10-08 15:10:46 -0400

    cart might work

--- videogamestore/frontend/videogame-ui/src/AddToCart.js
+++ videogamestore/frontend/videogame-ui/src/AddToCart.js
@@ -1,9 +1,33 @@
 import axios from 'axios';
 
+
+var addItemId = 0
 function addToCart(game) {
     const item = { id: game.id, gameTitle: game.title, quantity: 1, price: game.price };
 
     axios.post('/addtocart', item)
         .then(response => console.log(response))
         .catch(error => console.error(error));
+
+        addItemId += 1;
+
+        var selectedItem = document.createElement()
+        selectedItem.setAttribute(`id`, addItemId);
+        var price = document.createElement(`price`);
+        var cartItems = document.getElementsById(`name`);
+        name.innerText = game.player[1].innerText;
+        selectedItem.append(name);
+        selectedItem.append(price);
+        cartItems.append(selectedItem);
+    
+        var delBtn = document.createElement(`button`);
+        delBtn.innerText = `Delete`;
+        delBtn.setAttribute(`onclick`, `del(`+addItemId`)`);
+        selectedItem.append(delBtn)
 }
+        
+        function del(game){
+            document.getElementsById(game).remove();
+        }
+    
+    
