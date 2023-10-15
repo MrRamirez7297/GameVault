a0a1cbbf35ccf121bd5d5ea0cbb79f085c8286ef
Alex Ramirez <alexramirez7297@gmail.com>
2023-10-15 14:13:47 -0400

    changes

--- videogamestore/frontend/videogame-ui/src/Cart.js
+++ videogamestore/frontend/videogame-ui/src/Cart.js
@@ -1,33 +1,32 @@
-import React, { useState, useEffect  } from "react";
+import React, { useState, useEffect } from "react";
 import { useCart } from "./CartContext";
-import { BrowserRouter as Router, Route, Link} from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import "./Cart.css"
+import "./Cart.css";
 
 function Cart() {
-  const { cartItems, removeFromCart, updateQuantity} = useCart();
+  const { cartItems, removeFromCart, updateQuantity } = useCart();
   const [totalPrice, setTotalPrice] = useState(0);
 
   useEffect(() => {
-    
     cartItems.forEach((item) => {
       console.log("Item price:", item.price);
     });
 
-    const validCartItems = cartItems.filter((item) => !isNaN(parseFloat(item.price)));
+    const validCartItems = cartItems.filter(
+      (item) => !isNaN(parseFloat(item.price))
+    );
 
     const newTotalPrice = validCartItems.reduce((total, item) => {
       const itemPrice = parseFloat(item.price);
       return total + itemPrice * item.quantity;
     }, 0);
 
-
     //Debug
     console.log("New total price:", newTotalPrice);
 
     setTotalPrice(newTotalPrice);
   }, [cartItems]);
-  
 
   const handleRemoveFromCart = (itemId) => {
     removeFromCart(itemId);
@@ -43,44 +42,44 @@ function Cart() {
     </option>
   ));
 
-
-    return (
-        <div className='container'>
-            <h2 className='title-c'>Your Cart</h2>
-            {cartItems.length === 0 ? (
-      <p>Your Cart is Empty</p>
-    ) : (
-      <>
-            <ul>
-                {cartItems.map((item) => (
-                    <li className='list' key={item.id}>
-                    {item.name} - ${item.price}{" "}
-              <select
-                value={item.quantity}
-                onChange={(e) =>
-                  handleQuantityChange(item.id, parseInt(e.target.value, 10))
-                }
-              >
-                {quantityOptions}
-              </select>
-
-                        <button className='button1' onClick={() => handleRemoveFromCart(item.id)}>Remove</button>
-                    </li>
-                ))}
-            </ul>
-            <p className='price'>Total Price: ${totalPrice.toFixed(2)}</p>
-            <p>
-          <Link to="/shipping-page" state={{ totalPrice: totalPrice }}>
-            <button className="button2">Checkout</button>
-          </Link>
-        </p>
-      </>
-    )}
-
-        </div>
-    );
-    
-    
+  return (
+    <div className="container">
+      <h2 className="title-c">Your Cart</h2>
+      {cartItems.length === 0 ? (
+        <p>Your Cart is Empty</p>
+      ) : (
+        <>
+          <ul>
+            {cartItems.map((item) => (
+              <li className="list" key={item.id}>
+                {item.name} - ${item.price}{" "}
+                <select
+                  value={item.quantity}
+                  onChange={(e) =>
+                    handleQuantityChange(item.id, parseInt(e.target.value, 10))
+                  }
+                >
+                  {quantityOptions}
+                </select>
+                <button
+                  className="button1"
+                  onClick={() => handleRemoveFromCart(item.id)}
+                >
+                  Remove
+                </button>
+              </li>
+            ))}
+          </ul>
+          <p className="price">Total Price: ${totalPrice.toFixed(2)}</p>
+          <p>
+            <Link to="/shipping-page" state={{ totalPrice: totalPrice }}>
+              <button className="button2">Checkout</button>
+            </Link>
+          </p>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Cart;
