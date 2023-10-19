f834de45d38a38170accfe259270e4c6179b0664
Ahmad Omo-abu <Ahmadomo716@gmail.com>
2023-10-19 13:25:45 -0400

    search title 2

--- videogamestore/frontend/videogame-ui/src/Search.js
+++ videogamestore/frontend/videogame-ui/src/Search.js
@@ -10,14 +10,31 @@ function Search() {
   
   const gameTitleToIdMap = {
     "The Legend of Zelda: Tears of the Kingdom": 1,
+    "Zelda": 1,
+
     "Starfield": 2,
+
     "Diablo IV": 3,
+    "Diablo":3,
+
     "Baldur's Gate III": 4,
+    "Baldur's 3": 4,
+    "Baldur": 4,
+
     "Dead Island 2": 5,
+    "Dead Island": 5,
+    "dead island 2": 5,
+    "dead island": 5,
+
+
     "Atomic Heart": 6,
     "Star Wars Jedi: Survivor": 7,
     "Barotrauma": 8,
+
     "Final Fantasy XVI": 9,
+    "Final Fantasy": 9,
+    "final fantasy": 9,
+
     "Assassin's Creed Mirage": 10,
     "The Witcher 3: Wild Hunt": 11,
     "God of War (2018)": 12,
@@ -39,6 +56,28 @@ function Search() {
     "Red Dead Redemption": 28,
     "Red Dead Redemption 2": 29,
     "Star Wars: Knights of the Old Republic": 30,
+    "Call of Duty: Modern Warfare 2": 31,
+    "Bioshock Infinite": 32,
+
+    "Half-Life": 33,
+    "Borderlands": 34,
+    "Cyberpunk 2077":35,
+    "Horizon Zero Dawn": 36,
+    "TEKKEN 7": 37,
+    "NBA 2K20": 38,
+    "Tony Hawk's Pro Skater 1 + 2": 39,
+    "The Elder Scrolls V: Skyrim": 40,
+    "Tomb Raider (2013)": 41,
+    "DOOM (2016)": 42,
+    "Batman: Arkham Knight": 43,
+    "The Sims 4: Digital Deluxe Edition": 44,
+    "Sid Meier's Civilization V": 45,
+    "Stardew Valley": 46,
+    "NieR: Automata": 47,
+    "Portal 2": 48,
+    "Mario Kart 8 Deluxe": 49,
+    "Super Smash Bros Ultimate": 50,
+
     
     
   };
